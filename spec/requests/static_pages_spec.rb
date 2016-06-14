64d5e69876c579a5c85a4ef72a0b5418e859efa6
Nazedwox <nazedwox@gmail.com>
2016-06-15 00:53:24 +0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe "Static pages", type: :request do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the right title", type: :request do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
 
   describe "Help page", type: :request do
@@ -17,6 +22,11 @@ RSpec.describe "Static pages", type: :request do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the right title", type: :request do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
   end
 
   describe "About page", type: :request do
@@ -24,6 +34,11 @@ RSpec.describe "Static pages", type: :request do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title", type: :request do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
     end
   end
 end
